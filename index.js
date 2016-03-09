a1bf4966798f0f3926061d390d7e0c50604e1037
Andrea Parodi <andrea@parro.it>
2016-03-09 12:48:49 +0100

    more test done on run

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 'use strict';
 
-module.exports = function electronContextmenuMiddleware() {
-  return 42;
+const CoMws = require('comws');
+let context = new CoMws();
+
+
+exports.reset = function reset() {
+  context = new CoMws();
 };
+
+exports.use = function use(mw) {
+  if (typeof mw !== 'function') {
+    throw new Error('Function middleware argument required.');
+  }
+  context.use(mw);
+  return mw;
+};
+
+exports.activate = function activate() {
+
+};
+
+function run(elm, click) {
+  const menu = [];
+  return context
+    .run({menu, elm, click})
+    .then(() => menu);
+}
+
+exports.__test = { run };
+
--- test.js
+++ test.js
@@ -1,8 +1,85 @@
 const test = require('tape');
-const electronContextmenuMiddleware = require('./');
+const context = require('./');
 
-test('it work!', t => {
-  const result = electronContextmenuMiddleware();
-  t.equal(result, 42);
+test('has a use function', t => {
+  t.equal(typeof context.use, 'function');
   t.end();
 });
+
+test('has an activate function', t => {
+  t.equal(typeof context.use, 'function');
+  t.end();
+});
+
+
+test('use require a mw arg of type function', t => {
+  t.throws(() => context.use('no way'), /Function middleware argument required./);
+  t.end();
+});
+
+test('mws get a menu prop', t => {
+  context.reset();
+  context.use((ctx, next)=> {
+    t.ok(Array.isArray(ctx.menu));
+    t.end();
+    next();
+  });
+  context.__test.run({}, {});
+});
+
+
+test('mws get a elm prop', t => {
+  context.reset();
+  context.use((ctx, next)=> {
+    t.deepEqual(ctx.elm, {elm: true});
+    t.end();
+    next();
+  });
+  context.__test.run({elm: true}, {click: true})
+    .catch(err => t.end(err));
+
+});
+
+
+test('mws get a click prop', t => {
+  context.reset();
+  context.use((ctx, next)=> {
+    t.deepEqual(ctx.click, {click: true});
+    t.end();
+    next();
+  });
+  context.__test.run({elm: true}, {click: true})
+    .catch(err => t.end(err));
+});
+
+
+test('run return a promise to menu instance', t => {
+  context.reset();
+  context.use((ctx, next)=> {
+    next();
+  });
+  context.__test.run()
+    .then(menu => {
+      t.ok(Array.isArray(menu));
+      t.end();
+    })
+    .catch(err => t.end(err));
+
+});
+
+
+test('mws could alter menu instance', t => {
+  context.reset();
+  context.use((ctx, next)=> {
+    ctx.menu.push('test');
+    next();
+  });
+  context.__test.run()
+    .then(menu => {
+      t.deepEqual(menu, ['test']);
+      t.end();
+    })
+    .catch(err => t.end(err));
+
+});
+
