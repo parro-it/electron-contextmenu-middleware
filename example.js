7d41fd054cdd5249f09ce3284358bec39b1949c2
Andrea Parodi <andrea@parro.it>
2017-03-30 11:58:06 +0200

    some improvement for XO

--- example.js
+++ example.js
@@ -2,35 +2,35 @@
 const electron = require('electron');
 
 electron.app.on('ready', () => {
-  const win = new electron.BrowserWindow({
-    show: true
-  });
+	const win = new electron.BrowserWindow({
+		show: true
+	});
 
-  win.webContents.executeJavaScript(`
-    const context = require('${__dirname}');
+	win.webContents.executeJavaScript(`
+		const context = require('${__dirname}');
 
-    context.use((ctx, next) => {
-      ctx.menu.push({
-        label: 'Test 1',
-        click: () => alert('Test 1 clicked')
-      });
-      next();
-    });
+		context.use((ctx, next) => {
+			ctx.menu.push({
+				label: 'Test 1',
+				click: () => alert('Test 1 clicked')
+			});
+			next();
+		});
 
-    context.use((ctx, next) => {
-      if (ctx.elm.matches('input')) {
-        ctx.menu.push({
-          label: 'Test 2',
-          click: () => alert('Test 2 clicked')
-        });
-        next();
-      }
+		context.use((ctx, next) => {
+			if (ctx.elm.matches('input')) {
+				ctx.menu.push({
+					label: 'Test 2',
+					click: () => alert('Test 2 clicked')
+				});
+				next();
+			}
 
-    });
+		});
 
-    context.activate();
+		context.activate();
 
-  `);
+	`);
 
-  win.loadURL('https://google.com');
+	win.loadURL('https://google.com');
 });
--- index.js
+++ index.js
@@ -5,53 +5,53 @@ let context = new CoMws();
 
 
 function run(elm, click) {
-  const menu = [];
-  return context
-    .run({menu, elm, click})
-    .then(() => menu);
+	const menu = [];
+	return context
+		.run({menu, elm, click})
+		.then(() => menu);
 }
 
 function onContextmenu(e) {
-  const electron = require('electron');
-  const remote = electron.remote;
-  const Menu = remote.Menu;
-  const click = {x: e.x, y: e.y};
-  const elm = e.target;
+	const electron = require('electron');
+	const remote = electron.remote;
+	const Menu = remote.Menu;
+	const click = {x: e.x, y: e.y};
+	const elm = e.target;
 
-  run(elm, click)
-    .then(template => {
-      if (template.length > 0) {
-        e.preventDefault();
-        e.stopPropagation();
-        
-        const menu = Menu.buildFromTemplate(template);
-        menu.popup(remote.getCurrentWindow(), click.x, click.y);
-      }
+	run(elm, click)
+		.then(template => {
+			if (template.length > 0) {
+				e.preventDefault();
+				e.stopPropagation();
 
-    })
-    .catch(err => process.stderr.write(err.stack + '\n'));
+				const menu = Menu.buildFromTemplate(template);
+				menu.popup(remote.getCurrentWindow(), click.x, click.y);
+			}
+
+		})
+		.catch(err => process.stderr.write(err.stack + '\n'));
 }
 
 exports.reset = function reset() {
-  context = new CoMws();
+	context = new CoMws();
 };
 
 exports.use = function use(mw) {
-  if (typeof mw !== 'function') {
-    throw new Error('Function middleware argument required.');
-  }
-  context.use(mw);
-  return mw;
+	if (typeof mw !== 'function') {
+		throw new Error('Function middleware argument required.');
+	}
+	context.use(mw);
+	return mw;
 };
 
 exports.activate = function activate() {
-  if (document.body) {
-    document.body.addEventListener('contextmenu', onContextmenu);
-  } else {
-    document.addEventListener('DOMContentLoaded', () => {
-      document.body.addEventListener('contextmenu', onContextmenu);
-    });
-  }
+	if (document.body) {
+		document.body.addEventListener('contextmenu', onContextmenu);
+	} else {
+		document.addEventListener('DOMContentLoaded', () => {
+			document.body.addEventListener('contextmenu', onContextmenu);
+		});
+	}
 
 };
 
